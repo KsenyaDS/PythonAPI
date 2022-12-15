0c8ae380308cf623e294279ac4307443b2dcba20
Ksenya <davydova.kseniya@mail.ru>
2022-12-15 18:04:09 +0300

    update file hello.py

--- 1. hello/hello.py
+++ 1. hello/hello.py
@@ -1,9 +1,2 @@
-import requests
-import json
-string_json = '{"answer": "Hello from Ксения"}'
-obj = json.loads(string_json)
-key = "answer"
-if key in obj:
-    print(obj[key])
-else:
-    print(f"Ключа {key} в JSON нет")
+name = 'Ксения'
+print(f"Hello from {name}")
