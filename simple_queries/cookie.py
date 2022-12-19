da2a14de9411c960121f024b0677c59d2f8eb565
Ksenya <davydova.kseniya@mail.ru>
2022-12-19 15:42:00 +0300

    add files simple_queries

---
+++ simple_queries/cookie.py
@@ -0,0 +1,9 @@
+﻿import requests
+payload = {"login":"secret_login", "password":"secret_pass"}
+response = requests.post("https://playground.learnqa.ru/api/get_auth_cookie", data=payload)
+cookie_value = response.cookies.get('auth_cookie')
+cookies = {'auth_cookie': cookie_value}
+response2 = requests.post("https://playground.learnqa.ru/api/check_auth_cookie", cookies=cookies)
+print(dict(response.cookies))
+print(response.status_code)
+print(response2.text)
